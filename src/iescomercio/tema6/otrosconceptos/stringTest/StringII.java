c76546dff1b6fa967e36f181138434b316b96dc1
VESPERTINO <VESPERTINO@C8PC14>
2016-01-14 20:10:52 +0100

    adsf

--- src/iescomercio/tema6/otrosconceptos/stringTest/StringII.java
+++ src/iescomercio/tema6/otrosconceptos/stringTest/StringII.java
@@ -45,15 +45,84 @@ public class StringII {
         return s;
     }
 
+    //EJERCICIO 5
     public static void desordenar(String s) {
 
-        if (s.charAt(0) == '-') {
-            System.out.println("El numero es negativo");
-        } else if ((s.length()) % 2 != 0) {
-            System.out.println("Tiene que ser par");
-        } else {
-            
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') {
+                System.out.println("Solo se permiten numeros enteros o el cero.");
+            }
         }
+        if ((s.length()) % 2 != 0) {
+            System.out.println("Tiene que ser par");
+        }
+
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < s.length() / 2; i++) {
+            System.out.print(sb.charAt(i) + "" + sb.charAt(sb.length() - 1 - i));
+
+        }
+//        int i = (s.length() / 2);
+//        StringBuilder a = new StringBuilder();
+//        StringBuilder b = new StringBuilder();
+//        StringBuilder ab = new StringBuilder();
+//        a.append(s.substring(0, i));
+//        b.append(s.substring(i, s.length()));
+//        b.reverse();
+//
+//        for (int j = 0; j < a.length(); j++) {
+//            ab.append(a.charAt(j));
+//            ab.append(b.charAt(j));
+//        }
+//        System.out.println("El resultado es: " + ab);        
+    }
+
+    //EJERCICIO 6
+    public static String ejercicio6(String s1, String s2) {
+        StringBuilder sb = new StringBuilder();
+
+        if (s1.length() != s2.length()) {
+            System.out.println("Tienen que tener el mismo tamaño.");
+            return null;
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) < '0' || s1.charAt(i) > '9' || s2.charAt(i) < '0' || s2.charAt(i) > '9') {
+                System.out.println("Solo se permiten numeros enteros o el cero.");
+                return null;
+            }
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            sb.append(s1.charAt(i));
+            sb.append(s2.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static String ejercicio7(String s1, String s2) {
+        StringBuilder sb = new StringBuilder();
+
+        if (s1.length() % 3 != 0 || s2.length() % 3 != 0) {
+            System.out.println("El tamaño no es multiplo de 3");
+            return null;
+        }
+        if (s1.length() != s2.length()) {
+            System.out.println("Tienen que tener el mismo tamaño.");
+            return null;
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) < '0' || s1.charAt(i) > '9' || s2.charAt(i) < '0' || s2.charAt(i) > '9') {
+                System.out.println("Solo se permiten numeros enteros o el cero.");
+                return null;
+            }
+        }
+        for (int i = 0; i < s1.length(); i = i+2) {
+               
+            }
+
+        
+        return sb.toString();
 
     }
 }
--- src/iescomercio/tema6/otrosconceptos/stringTest/StringTest.java
+++ src/iescomercio/tema6/otrosconceptos/stringTest/StringTest.java
@@ -13,39 +13,15 @@ public class StringTest {
 
     public static void main(String[] args) {
         {
-
 //            System.out.println("Respuesta: " + StringII.esPalindromo("pepito") );
 //            System.out.println("Respuesta: " + StringII.contraseña("papepipopu") );
 //            System.out.println("Respuesta: " + StringII.esPalindromo("ossads") );
-            StringII.desordenar("151532");
-//
-//            String s1 = new String();
-//
-//            String s2 = new String(" ababABAB "); // Tiene espacios en blanco.
-//
-//            String s3 = "Ejemplo";
-//
-//            System.out.println(s1.compareTo(" ")); // “ “ es un espacio en blanco.
-//
-//            System.out.println(s2.concat((s3)));
-//
-//            System.out.println(s1.indexOf('h'));
-//
-//            System.out.println(s2.trim());
-//
-//            System.out.println(s2.toLowerCase());
-//
-//            System.out.println(s3.toUpperCase());
-//
-//            System.out.println(s2.indexOf('b'));
-//
-//            System.out.println(s2.lastIndexOf('b'));
-//
-//            System.out.println(s3.length());
-//
-//            System.out.println(s3.charAt(1));
-//
-//            System.out.println(s2.replace('A', 'x'));
+//            StringII.desordenar("0147");
+//            System.out.println(StringII.ejercicio6("874", "156"));
+//            System.out.println(StringII.ejercicio6("51", "11254"));
+//            System.out.println(StringII.ejercicio6("555", "-55"));
+            
+            System.out.println(StringII.ejercicio7("111222", "333444"));
         }
     }
 }
