fb257b4285a2d52c8256c4a37dcab7b6084a7624
Isabel <Isabel@Isa>
2016-02-02 14:55:50 +0100

    StringTokenizer

---
+++ src/iescomercio/tema7/arrays/ejercicio1/Ejercicio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iescomercio.tema7.arrays.ejercicio1;
+
+/**
+ *
+ * @author Isabel
+ */
+public class Ejercicio {
+    /*Implementar un programa en Java que cree e inicialice en instrucciones 
+    separadas un array de tipo float de tamaña 20.
+    Rellene dicho array con numeros aletatorios entre 0 y 10.
+    Y calcule y muestre por pantalla posteriormente la nota media de los elementos del array*/
+    
+    
+}
